69080a184ccf2bc2d817c21cbb6653dd67b9835f
Anton Barinoff <antonbarinoff1@gmail.com>
2025-11-01 19:19:12 +0700

    add file vb8_script2.sh

---
+++ vb8_script2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+
+
+echo "Введите путь к файлу:"
+read file_path
+
+if [ -f "$file_path" ]; then
+    echo "Файл найден!"
+else
+    echo "Файл не найден."
+fi
